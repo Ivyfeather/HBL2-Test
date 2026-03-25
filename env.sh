10996f5c408c0353889bc7e2549b9c6f6b272475
XiChen <chenxi171@mails.ucas.ac.cn>
2026-03-25 19:31:21 +0800

    misc: update run.sh and env.sh

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
 #set -x
-export LINUX=linux-kernel
-export RISCV_LINUX_HOME=$(pwd)/${LINUX}/linux-6.10.3
-export RISCV_ROOTFS_HOME=$(pwd)/${LINUX}/riscv-rootfs
-export WORKLOAD_BUILD_ENV_HOME=$(pwd)/${LINUX}/nemu_board
-export OPENSBI_HOME=$(pwd)/${LINUX}/opensbi
-export RISCV=/nfs/home/share/riscv # consider use llvm-stc with M-ext?
+export LINUX=$(pwd)/linux-kernel
+export RISCV_LINUX_HOME=${LINUX}/linux-6.10.3
+export RISCV_ROOTFS_HOME=${LINUX}/riscv-rootfs
+export WORKLOAD_BUILD_ENV_HOME=${LINUX}/nemu_board
+export OPENSBI_HOME=${LINUX}/opensbi
+export RISCV=/nfs/home/share/riscv                   # TODO: consider use llvm-stc with M-ext?
 
 export ARCH=riscv
 export CROSS_COMPILE=$RISCV/bin/riscv64-unknown-linux-gnu-
--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@ BASE=$(pwd)
 TESTS=${BASE}/Matrix-tests
 LINUX=${BASE}/linux-kernel
 NEMU=${BASE}/NEMU-Matrix
+TLTEST=${BASE}/tl-test-new-matrix
 
+THREADS_BUILD=${THREADS_BUILD:-$(($(nproc)/2))}
 M=0
 N=0
 L=0
@@ -14,13 +16,15 @@ while getopts "mnlr" OPT; do
         n) N=1;;
         l) L=1;;
         r) R=1;;
-        \?)
+	\?)
             echo "Unknown option: -$OPT"
-            echo "Usage: $0 [-m] [-n] [-l] [-r]"
+            echo "Usage: $0 [-m/n/l/r/c/t]"
             echo "  -m: build matrix workload"
             echo "  -n: build NEMU"
             echo "  -l: build linux kernel"
             echo "  -r: run workload on NEMU"
+	    echo "  -c: RTL and verilator compilation"
+	    echo "  -t: tl-test compilation and run"
             exit 1
             ;;
     esac
@@ -41,21 +45,24 @@ fi
 if [ $N -eq 1 ]; then
     echo "********** build NEMU **********"
     # make -C ${NEMU} riscv64-matrix-xs_defconfig
-    make -C ${NEMU} -j64
+    make -C ${NEMU} -j${THREADS_BUILD}
 fi
 
 # if [ ??? -eq 1 ]; then
     # echo "********** build workloads in rootfs **********"
-    # make -C ${LINUX}/riscv-rootfs -j64
+    # make -C ${LINUX}/riscv-rootfs -j${THREADS_BUILD}
 # fi
 
 if [ $L -eq 1 ]; then
+    # make -C ${LINUX}/linux-6.10.3 clean -j${THREADS_BUILD}
+    # make -C ${LINUX}/linux-6.10.3 xiangshan_defconfig -j${THREADS_BUILD}
+
     echo "********** build linux kernel **********"
     rm ${LINUX}/linux-6.10.3/vmlinux.o
-    make -C ${LINUX}/linux-6.10.3 -j64
+    make -C ${LINUX}/linux-6.10.3 -j${THREADS_BUILD}
 
     echo "********** build opensbi **********"
-    make -C ${LINUX}/opensbi PLATFORM=generic FW_PAYLOAD_PATH=$RISCV_LINUX_HOME/arch/riscv/boot/Image FW_FDT_PATH=$WORKLOAD_BUILD_ENV_HOME/dts/build/xiangshan.dtb FW_PAYLOAD_OFFSET=0x200000 -j64
+    make -C ${LINUX}/opensbi PLATFORM=generic FW_PAYLOAD_PATH=$RISCV_LINUX_HOME/arch/riscv/boot/Image FW_FDT_PATH=$WORKLOAD_BUILD_ENV_HOME/dts/build/xiangshan.dtb FW_PAYLOAD_OFFSET=0x200000 -j${THREADS_BUILD}
 fi
 
 if [ $R -eq 1 ]; then
@@ -65,5 +72,7 @@ if [ $R -eq 1 ]; then
 
     cd ${NEMU} && python3 addr.py && cd ${BASE}
     echo "addr.py parse"
+
+    cp ${NEMU}/line_trace.txt ${TLTEST}
 fi
 
